6ea7465507384d9adcff106ce799c9ba57eeab66
Pedronascentes1 <pedronasc002@gmail.com>
2026-02-28 12:42:22 -0300

    att 6.1.2

--- galvao-clean/src/pages/Login.tsx
+++ galvao-clean/src/pages/Login.tsx
@@ -11,7 +11,7 @@ export default function Login() {
     e.preventDefault()
 
     try {
-      const response = await fetch("http://localhost:3001/auth/login", {
+      const response = await fetch("https://galvaotech.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
